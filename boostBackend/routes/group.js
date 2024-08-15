f327c25956ab02198842e18b13f5c7325c5e4c27
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2024-08-15 15:09:32 +0900

    Feat: 그룹 수정 기능 추가

--- boostBackend/routes/group.js
+++ boostBackend/routes/group.js
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient(); 
 
 const groupRouter = express.Router();
+groupRouter.use(express.json());
 
 groupRouter.route('/')
   .post(async (req, res, next) => { // 그룹 등록 
@@ -34,6 +35,37 @@ groupRouter.route('/:groupId')
     const password = req.body.password; // 유저가 입력한 비밀번호
 
     if (isNaN(groupId) || !password) {
+      res.status(400).send({ message: "잘못된 요청입니다" });
+    }
+  
+    const findGroup = await prisma.password.findUnique({
+      where: {
+        groupId: groupId,
+      },
+    });
+  
+    if (!findGroup) {
+      res.status(404).send({ message: "존재하지 않습니다" });
+    }
+  
+    const realPassword = findGroup.password; // 원래 비밀번호 
+    if (password === realPassword) {
+      await prisma.group.delete({
+        where: {
+          id: groupId,
+        },
+      });
+      res.status(200).send({ message: "그룹 삭제 성공"});
+    } else {
+      res.status(403).send({ message: "비밀번호가 틀렸습니다"});
+    }
+    
+  })
+  .put(async (req, res) => { // 그룹 수정 
+    const groupId = Number(req.params.groupId); 
+    const password = req.body.password; 
+
+    if (isNaN(groupId) || !password) {
       return res.status(400).send({ message: "잘못된 요청입니다" });
     }
 
@@ -47,19 +79,23 @@ groupRouter.route('/:groupId')
       res.status(404).send({ message: "존재하지 않습니다" });
     }
 
-    const realPassword = findGroup.password;// 원래 비밀번호 
-  
+    const realPassword = findGroup.password; // 원래 비밀번호 
+    const { name, imageUrl, isPublic, introduction } = req.body;
+
     if (password === realPassword) {
-      await prisma.group.delete({
-        where: {
-          id: groupId,
-        },
+      const group = await prisma.group.update({
+        where: { id: groupId },
+        data: { 
+          name,
+          imageUrl,
+          isPublic,
+          introduction,
+         },
       });
-      res.status(200).send({ message: "그룹 삭제 성공"});
+      res.status(200).send(group);
     } else {
       res.status(403).send({ message: "비밀번호가 틀렸습니다"});
     }
-    
   });
 
 export default groupRouter;
