2b491183b1b264734d13e0afdc063cf63b4661b9
eugenienyp <162283488+eugenienyp@users.noreply.github.com>
2024-08-21 01:11:05 +0900

    댓글 라우터 추가

--- boostBackend/app.js
+++ boostBackend/app.js
@@ -1,11 +1,13 @@
 import express from 'express'; 
 import groupRouter from './routes/group.js';
 import postRouter from './routes/post.js';
+import commentRouter from './routes/comments.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/groups', groupRouter);
 app.use('/api/posts', postRouter); 
+app.use('/api/comments', commentRouter);
 
 app.listen(3000, () => console.log('server started'));
