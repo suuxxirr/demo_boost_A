9c6be61a2944c20357afeee643df2f6355b9b6d6
eugenienyp <162283488+eugenienyp@users.noreply.github.com>
2024-08-21 17:29:12 +0900

    badgeRouter 추가

--- boostBackend/app.js
+++ boostBackend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import groupRouter from './routes/group.js';
 import postRouter from './routes/post.js';
 import commentRouter from './routes/comment.js';
+import badgeRouter from './routes/badge.js';
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/api/groups', groupRouter);
 app.use('/api/posts', postRouter); 
 app.use('/api/comment', commentRouter);
+app.use('/api/badge', badgeRouter);
 
 app.listen(3000, () => console.log('server started'));
