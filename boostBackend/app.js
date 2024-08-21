c24158abc20c0372b581ec20d79423f35a619853
eugenienyp <162283488+eugenienyp@users.noreply.github.com>
2024-08-21 16:51:29 +0900

    Update app.js

--- boostBackend/app.js
+++ boostBackend/app.js
@@ -1,13 +1,13 @@
 import express from 'express'; 
 import groupRouter from './routes/group.js';
 import postRouter from './routes/post.js';
-import commentRouter from './routes/comments.js';
+import commentRouter from './routes/comment.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/groups', groupRouter);
 app.use('/api/posts', postRouter); 
-app.use('/api/comments', commentRouter);
+app.use('/api/comment', commentRouter);
 
 app.listen(3000, () => console.log('server started'));
