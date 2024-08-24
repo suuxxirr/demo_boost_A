435718f8505da14f78fe977b5772f3ef051df843
김민지 <144991060+minjiwkim@users.noreply.github.com>
2024-08-24 23:13:28 +0900

    Update app.js

--- boostBackend/app.js
+++ boostBackend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS 설정
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
