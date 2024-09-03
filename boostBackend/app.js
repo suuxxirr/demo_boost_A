fc7d71bee160c32b221841fa74d8e05a8f190348
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2024-09-03 11:44:04 +0900

    docs: 문서 수정

--- boostBackend/app.js
+++ boostBackend/app.js
@@ -8,12 +8,13 @@ import cors from 'cors';
 const app = express();
 
 // CORS 설정
-app.use(cors({
-  origin: '*',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true,
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: '*',
+//   methods: 'GET,POST,PUT,DELETE',
+//   allowedHeaders: 'Content-Type,Authorization',
+//   credentials: true,
+// }));
 
 app.use(express.json());
 
